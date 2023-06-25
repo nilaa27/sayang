63b51ba07a537cd02b9172000f532259d5403987
nilaa27 <85550079+nilaa27@users.noreply.github.com>
2023-06-25 16:43:34 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -235,6 +235,9 @@ echo -e "\e[33m━━━━━━━━━━━━━━━━━━━━━�
 sleep 2
 clear
 wget https://raw.githubusercontent.com/nilaa27/sayang/main/ssh/ssh-vpn.sh && chmod +x ssh-vpn.sh && ./ssh-vpn.sh
+sleap 2
+clear
+wget https://raw.githubusercontent.com/xlord27/gabut/main/nginx-ssl.sh && chmod +x nginx-ssl.sh && ./nginx-ssl.sh
 #install Backup
 echo -e "\e[33m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
 echo -e "$green      Install BACKUP           $NC"
