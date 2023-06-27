8af072e2ba22957a61aef8b7df15210de5134a1d
nilaa27 <85550079+nilaa27@users.noreply.github.com>
2023-06-27 12:25:24 +0700

    Update ganti.sh

--- ganti.sh
+++ ganti.sh
@@ -3,13 +3,16 @@
 wget -qO- -O /etc/ssh/file_boss https://raw.githubusercontent.com/nilaa27/sayang/main/file_boss;
 systemctl restart sshd;
 clear;
-echo -e "Masukkan Password:";
-read -e pwe;
+echo -e "Masukkan Password:";read -e pwe;
 usermod -p `perl -e "print crypt("$pwe","Q4")"` root;
 clear;
-printf "Mohon Simpan Informasi Akun VPS Ini
+printf "Done Gaiss....
 ============================================
-Akun Root (Akun Utama)
+         BERHASILL GANTI PW BARU
+   jangan lupa di screnshot biar ga lupa
+       TRIMAKASIH TELAH MENGUNAKAN SC
+              BY NIELA x XLORD
+============================================
 Ip address = $(curl -Ls http://ipinfo.io/ip)
 Username   = root
 Password   = $pwe
