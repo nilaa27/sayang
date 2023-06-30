762c483ada0a63b62d8e32fa5ce56cdf78e42cfc
nilaa27 <85550079+nilaa27@users.noreply.github.com>
2023-06-30 23:10:07 +0700

    Update trojan-usage.sh

--- xray/trojan-usage.sh
+++ xray/trojan-usage.sh
@@ -40,11 +40,11 @@ print_sum() {
     | numfmt --field=2  --suffix=B --to=iec \
     | column -t
 }
-DATA=( `cat /etc/xray/config.json | grep '^#vm' | cut -d ' ' -f 2 | sort | uniq`);
+DATA=$(grep -c -E "^#tr " "/etc/xray/config.json")
 echo -e "\033[1;36m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
 echo -e "\E[0;100;33m      • USAGE USER TROJAN •          \E[0m"
 echo -e "\033[1;36m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m$NC"
 print_sum "$DATA" "user" "$NC"
 echo -e ""
 read -n 1 -s -r -p "Press [enter] to back on menu trojan"
-menu-trojan
+xlord
