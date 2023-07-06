d949517f33cd7e0bd99722447d7e16d9a4e179fe
nilaa27 <85550079+nilaa27@users.noreply.github.com>
2023-07-06 19:46:34 +0700

    Update menu-trojan.sh

--- menu/menu-trojan.sh
+++ menu/menu-trojan.sh
@@ -120,6 +120,10 @@ sed -i '/#trojangrpc$/a\#trg '"$user $exp"'\
 trojanlink1="trojan://${uuid}@${domain}:443?mode=gun&security=tls&type=grpc&serviceName=trojan-grpc&sni=${domain}#${user}"
 trojanlink="trojan://${uuid}@${domain}:443?path=%2Ftrojan-ws&security=tls&host=$sni&type=ws&sni=${domain}#${user}"
 
+xlord1="trojan://${uuid}@${domain}:443?mode=gun&security=tls&type=grpc&serviceName=trojan-grpc&sni=${domain}#${user} | base64 -w 0)"
+xlord2="trojan://${uuid}@${domain}:443?path=%2Ftrojan-ws&security=tls&host=$sni&type=ws&sni=${domain}#${user} | base64 -w 0)"
+
+
 TEXT="
 <code>──────────────────────</code>
 <code>    Xray TROJAN Account</code>
@@ -139,10 +143,10 @@ TEXT="
 <code>Path GRPC    : </code> <code>/trojan-grpc</code>
 <code>──────────────────────</code>
 <code>Link TLS     :</code> 
-<code>${trojanlink}</code>
+<code>${xlord2}</code>
 <code>──────────────────────</code>
 <code>Link GRPC    :</code> 
-<code>${trojanlink1}</code>
+<code>${xlord1}</code>
 <code>──────────────────────</code>
 <code>Created      : $harini</code>
 <code>──────────────────────</code>
