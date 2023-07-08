dc335746dda5020a824339104497e0b17edbe357
nilaa27 <85550079+nilaa27@users.noreply.github.com>
2023-07-08 23:10:18 +0700

    Update ohp.sh

--- OPENVPN/ohp.sh
+++ OPENVPN/ohp.sh
@@ -1,90 +1,118 @@
 #!/bin/bash
-#Open HTTP Puncher By DIYVPN
-#Direct Proxy Squid For OpenVPN TCP
-RED='\e[1;31m'
-GREEN='\e[0;32m'
-BLUE='\e[0;34m'
-NC='\e[0m'
-MYIP=$(wget -qO- https://icanhazip.com);
-MYIP2="s/xxxxxxxxx/$MYIP/g";
-domain=$(cat /etc/xray/domain)
-#Update Repository VPS
-clear
-apt update 
-apt-get -y upgrade
+# Ohp Script
+# ==========================================
+# Color
+RED='\033[0;31m'
+NC='\033[0m'
+GREEN='\033[0;32m'
+ORANGE='\033[0;33m'
+BLUE='\033[0;34m'
+PURPLE='\033[0;35m'
+CYAN='\033[0;36m'
+LIGHT='\033[0;37m'
+# ==========================================
+# Getting
 
-#Port Server ovpn ohp
-#Jika Ingiin Mengubah Port Silahkan Sesuaikan Dengan Port Yang Ada Di VPS Mu
-Port_OpenVPN_TCP='1194';
-Port_Squid='3128';
-Port_OHP='8787';
+# Download File Ohp
+https://raw.githubusercontent.com/nilaa26/sayang/main/OPENVPN/ohpserver-linux32.zip
+unzip ohpserver-linux32.zip
+chmod +x ohpserver
+cp ohpserver /usr/local/bin/ohpserver
+/bin/rm -rf ohpserver*
 
-#Installing ohp Server
-cd 
-wget -O /usr/local/bin/ohp " https://raw.githubusercontent.com/nilaa27/sayang/main/OPENVPN/ohp"
-chmod +x /usr/local/bin/ohp
-
-#Buat File OpenVPN TCP OHP
-cat > /etc/openvpn/client-tcp-ohp1194.ovpn <<END
-############## WELCOME ###############
-############# By DIYVPN ##############
-client
-dev tun
-proto tcp
-remote "bug.com" 1194
-resolv-retry infinite
-route-method exe
-nobind
-remote-cert-tls server
-cipher AES-256-CBC
-auth SHA256
-persist-key
-persist-tun
-auth-user-pass
-comp-lzo
-verb 3
-
-setenv FRIENDLY_NAME "TCP OHP VPN"
-http-proxy ${domain} 8787
-http-proxy-option CUSTOM-HEADER CONNECT HTTP/1.1
-http-proxy-option CUSTOM-HEADER Host bug.com
-http-proxy-option CUSTOM-HEADER X-Online-Host bug.com
-http-proxy-option CUSTOM-HEADER X-Forward-Host bug.com
-http-proxy-option CUSTOM-HEADER Connection: keep-alive
-END
-
-sed -i $MYIP2 /etc/openvpn/client-tcp-ohp1194.ovpn;
-
-# masukkan certificatenya ke dalam config client TCP 1194
-echo '<ca>' >> /etc/openvpn/client-tcp-ohp1194.ovpn
-cat /etc/openvpn/server/ca.crt >> /etc/openvpn/client-tcp-ohp1194.ovpn
-echo '</ca>' >> /etc/openvpn/client-tcp-ohp1194.ovpn
-cp /etc/openvpn/client-tcp-ohp1194.ovpn /home/vps/public_html/client-tcp-ohp1194.ovpn
-clear
-cd 
-
-#Buat Service Untuk OHP Ovpn
-cat > /etc/systemd/system/ohp.service <<END
+# Installing Service
+# SSH OHP Port 8181
+cat > /etc/systemd/system/ssh-ohp.service << END
 [Unit]
-Description=Direct Squid Proxy For OpenVPN TCP By DIYVPN
-Documentation=https://t.me/@Baung2012
-Wants=network.target
-After=network.target
+Description=SSH OHP Redirection Service
+Documentation=https://t.me/Hendra2012
+After=network.target nss-lookup.target
 
 [Service]
-ExecStart=/usr/local/bin/ohp -port 8787 -proxy 127.0.0.1:3128 -tunnel 127.0.0.1:1194
-Restart=always
-RestartSec=3
+Type=simple
+User=root
+CapabilityBoundingSet=CAP_NET_ADMIN CAP_NET_BIND_SERVICE
+AmbientCapabilities=CAP_NET_ADMIN CAP_NET_BIND_SERVICE
+NoNewPrivileges=true
+ExecStart=/usr/local/bin/ohpserver -port 8181 -proxy 127.0.0.1:3128 -tunnel 127.0.0.1:22
+Restart=on-failure
+LimitNOFILE=infinity
+
+[Install]
+WantedBy=multi-user.target
+END
+
+# Dropbear OHP 8282
+cat > /etc/systemd/system/dropbear-ohp.service << END
+[Unit]]
+Description=Dropbear OHP Redirection Service
+Documentation=https://t.me/Hendra2012
+After=network.target nss-lookup.target
+
+[Service]
+Type=simple
+User=root
+CapabilityBoundingSet=CAP_NET_ADMIN CAP_NET_BIND_SERVICE
+AmbientCapabilities=CAP_NET_ADMIN CAP_NET_BIND_SERVICE
+NoNewPrivileges=true
+ExecStart=/usr/local/bin/ohpserver -port 8282 -proxy 127.0.0.1:3128 -tunnel 127.0.0.1:109
+Restart=on-failure
+LimitNOFILE=infinity
+
+[Install]
+WantedBy=multi-user.target
+END
+
+# OpenVPN OHP 8383
+cat > /etc/systemd/system/openvpn-ohp.service << END
+[Unit]]
+Description=OpenVPN OHP Redirection Service
+Documentation=https://t.me/Baung2012
+After=network.target nss-lookup.target
+
+[Service]
+Type=simple
+User=root
+CapabilityBoundingSet=CAP_NET_ADMIN CAP_NET_BIND_SERVICE
+AmbientCapabilities=CAP_NET_ADMIN CAP_NET_BIND_SERVICE
+NoNewPrivileges=true
+ExecStart=/usr/local/bin/ohpserver -port 8383 -proxy 127.0.0.1:3128 -tunnel 127.0.0.1:1194
+Restart=on-failure
+LimitNOFILE=infinity
 
 [Install]
 WantedBy=multi-user.target
 END
 
 systemctl daemon-reload
-systemctl enable ohp
-systemctl restart ohp
-echo ""
-echo -e "${GREEN}Done Installing OHP Server${NC}"
-echo -e "Port OVPN OHP TCP: $Port_OHP"
-echo -e "Link Download OVPN OHP: http://$MYIP:81/client-tcp-ohp1194.ovpn"
-echo -e "Script By Tarap-Kuhing"
+systemctl enable ssh-ohp
+systemctl restart ssh-ohp
+systemctl enable dropbear-ohp
+systemctl restart dropbear-ohp
+systemctl enable openvpn-ohp
+systemctl restart openvpn-ohp
+#------------------------------
+printf 'INSTALLATION COMPLETED !\n'
+sleep 0.5
+printf 'CHECKING LISTENING PORT\n'
+if [ -n "$(ss -tupln | grep ohpserver | grep -w 8181)" ]
+then
+	echo 'SSH OHP Redirection Running'
+else
+	echo 'SSH OHP Redirection Not Found, please check manually'
+fi
+sleep 0.5
+if [ -n "$(ss -tupln | grep ohpserver | grep -w 8282)" ]
+then
+	echo 'Dropbear OHP Redirection Running'
+else
+	echo 'Dropbear OHP Redirection Not Found, please check manually'
+fi
+sleep 0.5
+if [ -n "$(ss -tupln | grep ohpserver | grep -w 8383)" ]
+then
+	echo 'OpenVPN OHP Redirection Running'
+else
+	echo 'OpenVPN OHP Redirection Not Found, please check manually'
+fi
+sleep 0.5
