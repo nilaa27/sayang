b5fc973189ea17dbd5143841f411c3ee9a64a6c8
nilaa27 <85550079+nilaa27@users.noreply.github.com>
2023-07-08 10:27:41 +0700

    Update about.sh

--- menu/about.sh
+++ menu/about.sh
@@ -1,5 +1,68 @@
 #!/bin/bash
-#Script Premium xlord
+
+BURIQ () {
+    curl -sS https://raw.githubusercontent.com/nilaa27/cinta/main/vps> /root/tmp
+    data=( `cat /root/tmp | grep -E "^### " | awk '{print $2}'` )
+    for user in "${data[@]}"
+    do
+    exp=( `grep -E "^### $user" "/root/tmp" | awk '{print $3}'` )
+    d1=(`date -d "$exp" +%s`)
+    d2=(`date -d "$biji" +%s`)
+    exp2=$(( (d1 - d2) / 86400 ))
+    if [[ "$exp2" -le "0" ]]; then
+    echo $user > /etc/.$user.ini
+    else
+    rm -f /etc/.$user.ini > /dev/null 2>&1
+    fi
+    done
+    rm -f /root/tmp
+}
+
+MYIP=$(curl -sS ipv4.icanhazip.com)
+Name=$(curl -sS https://raw.githubusercontent.com/nilaa27/cinta/main/vps| grep $MYIP | awk '{print $2}')
+echo $Name > /usr/local/etc/.$Name.ini
+CekOne=$(cat /usr/local/etc/.$Name.ini)
+
+Bloman () {
+if [ -f "/etc/.$Name.ini" ]; then
+CekTwo=$(cat /etc/.$Name.ini)
+    if [ "$CekOne" = "$CekTwo" ]; then
+        res="Expired"
+    fi
+else
+res="Permission Accepted..."
+fi
+}
+
+PERMISSION () {
+    MYIP=$(curl -sS ipv4.icanhazip.com)
+    IZIN=$(curl -sS https://raw.githubusercontent.com/nilaa27/cinta/main/vps| awk '{print $4}' | grep $MYIP)
+    if [ "$MYIP" = "$IZIN" ]; then
+    Bloman
+    else
+    res="Permission Denied!"
+    fi
+    BURIQ
+}
+red='\e[1;31m'
+green='\e[1;32m'
+NC='\e[0m'
+green() { echo -e "\\033[32;1m${*}\\033[0m"; }
+red() { echo -e "\\033[31;1m${*}\\033[0m"; }
+PERMISSION
+
+if [ "$res" = "Expired" ]; then
+Exp="\e[36mExpired\033[0m"
+else
+Exp=$(curl -sS https://raw.githubusercontent.com/nilaa27/cinta/main/vps| grep $MYIP | awk '{print $3}')
+fi
+
+###########- END COLOR CODE -##########
+tram=$( free -h | awk 'NR==2 {print $2}' )
+uram=$( free -h | awk 'NR==2 {print $3}' )
+pro=$(curl -s ipinfo.io/org | cut -d " " -f 2-10 )
+kota=$(curl -s ipinfo.io/city )
+
 
 BIBlack='\033[1;90m'      # Black
 BIRed='\033[1;91m'        # Red
@@ -16,7 +79,7 @@ IBlack='\033[0;90m'       # Black
 IRed='\033[0;91m'         # Red
 IGreen='\033[0;92m'       # Green
 IYellow='\033[0;93m'      # Yellow
-IBlue='\033[0;94m'        # Blue
+biru='\033[0;94m'        # Blue
 IPurple='\033[0;95m'      # Purple
 ICyan='\033[0;96m'        # Cyan
 IWhite='\033[0;97m'       # White
@@ -48,11 +111,11 @@ export WARNING="${RED}\e[5m"
 export UNDERLINE="\e[4m"
 
 clear
-echo -e "\e[1;32m══════════════════ Autoscript PREMIUM ══════════════════\e[0m" | tee -a log-install.txt
+echo -e "$biru══════════════════ Autoscript PREMIUM ══════════════════\e[0m" | tee -a log-install.txt
 echo ""  | tee -a log-install.txt
-echo "   >>> Service & Port"  | tee -a log-install.txt
+echo "   >>> Service & Port" | lolcat  | tee -a log-install.txt
 echo ""  | tee -a log-install.txt
-echo "   $NC    ${sc} [INFORMASI SSH SERVICE PORT] ${NC}" | lolcat | tee -a log-install.txt
+echo "   $NC   \e[1;97;101m        << INFORMASI PORT SSH >>        \e[0m ${NC}" | tee -a log-install.txt
 echo "   - ——————————————————————————————————————"
 echo "   - OpenSSH                 : 22"  | tee -a log-install.txt
 echo "   - Stunnel4                : 443"  | tee -a log-install.txt
@@ -60,13 +123,13 @@ echo "   - Udp Custom              : 1-65535"  | tee -a log-install.txt
 echo "   - Websocket SSH(HTTP)     : 80, 8880"  | tee -a log-install.txt
 echo "   - Websocket SSL(HTTPS)    : 443"  | tee -a log-install.txt
 echo ""  | tee -a log-install.txt
-echo "   $NC    ${sc} [INFORMASI Grpc, Bdvp, Ngnx] ${NC}"  | lolcat | tee -a log-install.txt
+echo "   $NC    \e[1;97;101m        << BDVPN,NGNX,GRPC >>        \e[0m ${NC}"  | tee -a log-install.txt
 echo "   - ——————————————————————————————————————"
 echo "   - Badvpn                  : 7200"  | tee -a log-install.txt
 echo "   - Nginx                   : 81"  | tee -a log-install.txt
 echo "   - Grpc                    : 443"  | tee -a log-install.txt
 echo ""  | tee -a log-install.txt
-echo "   $NC    ${sc} [INFORMASI XRAY] ${NC}"  | lolcat  | tee -a log-install.txt
+echo "   $NC     \e[1;97;101m        << XRAY PORT >>        \e[0m ${NC}"    | tee -a log-install.txt
 echo "   - ——————————————————————————————————————"
 echo "   - Xray Vmess Ws Tls       : 443"  | tee -a log-install.txt
 echo "   - Xray Vless Ws Tls       : 443"  | tee -a log-install.txt
@@ -74,7 +137,7 @@ echo "   - Xray Trojan Ws Tls      : 443"  | tee -a log-install.txt
 echo "   - Xray Vmess Ws None Tls  : 80, 8080"  | tee -a log-install.txt
 echo "   - Xray Vless Ws None Tls  : 80, 8080"  | tee -a log-install.txt
 echo "   - ——————————————————————————————————————"
-echo "   -$NC    ${sc} [ALL SERVICE SC] ${NC}"  | lolcat | tee -a log-install.txt
+echo "   -$NC   \e[1;97;101m        << SERVICE SCRIPT >>        \e[0m  ${NC}"  | tee -a log-install.txt
 echo""
 echo "   - ✅BW MONITORING"  | tee -a log-install.txt
 echo "   - ✅CHECK LOGIN USER"  | tee -a log-install.txt
@@ -87,7 +150,7 @@ echo "   - ✅MENU TRIAL ALL SERVICE"  | tee -a log-install.txt
 echo "   - ✅GANTI PW VPS"  | tee -a log-install.txt
 echo""
 echo "   - ———————————————————————————————————"
-echo "      $NC    ${sc}     DETAIL CLIENTS    ${NC}"  | lolcat
+echo "      $NC      DETAIL CLIENTS    ${NC}"  | lolcat
 echo "   - ———————————————————————————————————"
 echo "   - Clients:${Name}"
 echo "   - EXP:${Exp}"
@@ -95,7 +158,7 @@ echo "   - ISP:${pro}"
 echo "   - CITY:${kota}"
       echo -e                                                                           
 echo ""  | tee -a log-install.txt
-echo -e "\e[1;32m══════════════════ Autoscript By XlordVpn ══════════════════\e[0m" | tee -a log-install.txt
+echo -e "$biru══════════════════ Autoscript By XlordVpn ══════════════════\e[0m" | tee -a log-install.txt
 echo ""
 read -n 1 -s -r -p "Press any key to back on menu"
 xlord
