fc9ea97f4698a076da753a7c58136058b086f0bc
nilaa27 <85550079+nilaa27@users.noreply.github.com>
2023-07-10 12:39:47 +0700

    Update xp.sh

--- ssh/xp.sh
+++ ssh/xp.sh
@@ -2,44 +2,44 @@
 red() { echo -e "\\033[32;1m${*}\\033[0m"; }
 clear
 ##----- Auto Remove Vmess
-data=($(cat /etc/xray/config.json | grep '^###' | cut -d ' ' -f 2 | sort | uniq))
+data=($(cat /etc/xray/config.json | grep "^#vma " | cut -d ' ' -f 2 | sort | uniq))
 now=$(date +"%Y-%m-%d")
 for user in "${data[@]}"; do
-    exp=$(grep -w "^### $user" "/etc/xray/config.json" | cut -d ' ' -f 3 | sort | uniq)
+    exp=$(grep -w "^#vma# $user" "/etc/xray/config.json" | cut -d ' ' -f 3 | sort | uniq)
     d1=$(date -d "$exp" +%s)
     d2=$(date -d "$now" +%s)
     exp2=$(((d1 - d2) / 86400))
     if [[ "$exp2" -le "0" ]]; then
-        sed -i "/^### $user $exp/,/^},{/d" /etc/xray/config.json
-        sed -i "/^### $user $exp/,/^},{/d" /etc/xray/config.json
+        sed -i "/^#vma# $user $exp/,/^},{/d" /etc/xray/config.json
+        sed -i "/^#vma# $user $exp/,/^},{/d" /etc/xray/config.json
     fi
 done
 
 #----- Auto Remove Vless
-data=($(cat /etc/xray/config.json | grep '^#&' | cut -d ' ' -f 2 | sort | uniq))
+data=($(cat /etc/xray/config.json | grep "^#vl " | cut -d ' ' -f 2 | sort | uniq))
 now=$(date +"%Y-%m-%d")
 for user in "${data[@]}"; do
-    exp=$(grep -w "^#& $user" "/etc/xray/config.json" | cut -d ' ' -f 3 | sort | uniq)
+    exp=$(grep -w "^#vl# $user" "/etc/xray/config.json" | cut -d ' ' -f 3 | sort | uniq)
     d1=$(date -d "$exp" +%s)
     d2=$(date -d "$now" +%s)
     exp2=$(((d1 - d2) / 86400))
     if [[ "$exp2" -le "0" ]]; then
-        sed -i "/^#& $user $exp/,/^},{/d" /etc/xray/config.json
-        sed -i "/^#& $user $exp/,/^},{/d" /etc/xray/config.json
+        sed -i "/^#vl# $user $exp/,/^},{/d" /etc/xray/config.json
+        sed -i "/^#vl# $user $exp/,/^},{/d" /etc/xray/config.json
     fi
 done
 
 #----- Auto Remove Trojan
-data=($(cat /etc/xray/config.json | grep '^#!' | cut -d ' ' -f 2 | sort | uniq))
+data=($(cat /etc/xray/config.json | grep "^#tr " | cut -d ' ' -f 2 | sort | uniq))
 now=$(date +"%Y-%m-%d")
 for user in "${data[@]}"; do
-    exp=$(grep -w "^#! $user" "/etc/xray/config.json" | cut -d ' ' -f 3 | sort | uniq)
+    exp=$(grep -w "^#tr# $user" "/etc/xray/config.json" | cut -d ' ' -f 3 | sort | uniq)
     d1=$(date -d "$exp" +%s)
     d2=$(date -d "$now" +%s)
     exp2=$(((d1 - d2) / 86400))
     if [[ "$exp2" -le "0" ]]; then
-        sed -i "/^#! $user $exp/,/^},{/d" /etc/xray/config.json
-        sed -i "/^#! $user $exp/,/^},{/d" /etc/xray/config.json
+        sed -i "/^#tr# $user $exp/,/^},{/d" /etc/xray/config.json
+        sed -i "/^#tr# $user $exp/,/^},{/d" /etc/xray/config.json
     fi
 done
 
