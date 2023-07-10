7925eee746ce0c9d72eb415c3ac5889a5c92e716
nilaa27 <85550079+nilaa27@users.noreply.github.com>
2023-07-10 16:06:38 +0700

    Update limit.sh

--- ssh/limit.sh
+++ ssh/limit.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-wget -q -O /etc/systemd/system/limitvmess.service "https://raw.githubusercontent.com/nilaa27/main/ssh/limitvmess.service" && chmod +x limitvmess.service >/dev/null 2>&1
-wget -q -O /etc/systemd/system/limitvless.service "https://raw.githubusercontent.com/nilaa27/main/ssh/limitvless.service" && chmod +x limitvless.service >/dev/null 2>&1
-wget -q -O /etc/systemd/system/limittrojan.service "https://raw.githubusercontent.com/nilaa27/main/ssh/limittrojan.service" && chmod +x limittrojan.service >/dev/null 2>&1
-wget -q -O /etc/systemd/system/limitshadowsocks.service "https://raw.githubusercontent.com/nilaa27/main/ssh/limitshadowsocks.service" && chmod +x limitshadowsocks.service >/dev/null 2>&1
-wget -q -O /etc/limit/vmess/ "https://raw.githubusercontent.com/nilaa27/main/ssh/vmess" >/dev/null 2>&1
-wget -q -O /etc/limit/vless/ "https://raw.githubusercontent.com/nilaa27/main/ssh/vless" >/dev/null 2>&1
-wget -q -O /etc/limit/trojan/ "https://raw.githubusercontent.com/nilaa27/main/ssh/trojan" >/dev/null 2>&1
-wget -q -O /etc/xray/limit.shadowsocks "https://raw.githubusercontent.com/nilaa27/main/ssh/shadowsocks" >/dev/null 2>&1
+wget -q -O /etc/systemd/system/limitvmess.service "https://raw.githubusercontent.com/nilaa27/main/ssh/limitvmess.service" && chmod +x limitvmess.service 
+wget -q -O /etc/systemd/system/limitvless.service "https://raw.githubusercontent.com/nilaa27/main/ssh/limitvless.service" && chmod +x limitvless.service 
+wget -q -O /etc/systemd/system/limittrojan.service "https://raw.githubusercontent.com/nilaa27/main/ssh/limittrojan.service" && chmod +x limittrojan.service 
+wget -q -O /etc/systemd/system/limitshadowsocks.service "https://raw.githubusercontent.com/nilaa27/main/ssh/limitshadowsocks.service" && chmod +x limitshadowsocks.service 
+wget -q -O /etc/limit/vmess/ "https://raw.githubusercontent.com/nilaa27/main/ssh/vmess" 
+wget -q -O /etc/limit/vless/ "https://raw.githubusercontent.com/nilaa27/main/ssh/vless" 
+wget -q -O /etc/limit/trojan/ "https://raw.githubusercontent.com/nilaa27/main/ssh/trojan" 
+wget -q -O /etc/xray/limit.shadowsocks "https://raw.githubusercontent.com/nilaa27/main/ssh/shadowsocks" 
 chmod +x /etc/limit/vmess/
 chmod +x /etc/limit/vless/
 chmod +x /etc/limit/trojan/
